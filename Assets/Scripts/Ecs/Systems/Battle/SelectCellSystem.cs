b65bda12885d1f1adcf247b464d3ffc418a56910
vlifirenko <vladislav.lifirenko@gmail.com>
2023-05-26 16:08:26 +0300

    Select cell system

--- Assets/Scripts/Ecs/Systems/Battle/SelectCellSystem.cs
+++ Assets/Scripts/Ecs/Systems/Battle/SelectCellSystem.cs
@@ -1,20 +1,41 @@
 ﻿using Scellecs.Morpeh;
 using TurnBasedRPG.Installers;
+using UnityEngine;
 
 namespace TurnBasedRPG.Ecs.Systems.Battle
 {
     public class SelectCellSystem : ISystem
     {
         private readonly GlobalConfigInstaller.LayersConfig _layersConfig;
-        
+        private readonly RaycastHit[] _raycastHits = new RaycastHit[1];
+
         public World World { get; set; }
 
-        public void OnAwake() {}
+        public SelectCellSystem(GlobalConfigInstaller.LayersConfig layersConfig)
+        {
+            _layersConfig = layersConfig;
+        }
 
-        public void OnUpdate(float deltaTime)
+        public void OnAwake()
         {
         }
 
-        public void Dispose() {}
+        public void OnUpdate(float deltaTime)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hits = Physics.RaycastNonAlloc(ray, _raycastHits, Mathf.Infinity, _layersConfig.cell);
+            Debug.Log(hits);
+            if (hits == 0)
+                return;
+            
+            for (var i = 0; i < hits; i++)
+            {
+                Debug.Log("Hit " + _raycastHits[i].collider.gameObject.name);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
     }
 }
